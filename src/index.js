f56e6ebe0f8ff18a47344c3493fbf08799259104
jyyang <1761398181@qq.com>
2022-09-11 22:01:22 +0800

    add react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from './routes/invoice';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='expenses' element={<Expenses />}></Route>
+        <Route path='invoices' element={<Invoices />}>
+        <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>    
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
